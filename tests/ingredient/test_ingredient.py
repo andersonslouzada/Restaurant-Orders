c0f0ef9a0760b62d0057153d9efd546fd1251b15
andersonlouzada <anderson.s.louzada@gmail.com>
2023-11-10 10:25:16 -0300

    test: create tests to class Ingredient

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,16 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 
-# Req 1
 def test_ingredient():
-    pass
+    ingredient = Ingredient("queijo mussarela")
+    assert ingredient.name == "queijo mussarela"
+
+    restrictions = {Restriction.LACTOSE, Restriction.ANIMAL_DERIVED}
+    assert ingredient.restrictions == restrictions
+
+    assert ingredient.__repr__() == "Ingredient('queijo mussarela')"
+
+    ingredient2 = Ingredient("queijo mussarela")
+    assert ingredient.__eq__(ingredient2) is True
+
+    assert ingredient.__hash__() == hash(ingredient.name)
