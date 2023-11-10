caf667173ee2278e80b264c427b054a85fe4143f
andersonlouzada <anderson.s.louzada@gmail.com>
2023-11-10 11:29:44 -0300

    feat: create class MenuData

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,27 @@
-# Req 3
+import csv
+from typing import Set
+from src.models.dish import Dish
+from src.models.ingredient import Ingredient
+
+
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.dishes: Set[Dish] = set()
+        self._read_data(source_path)
+
+    def _read_data(self, source_path: str) -> None:
+        with open(source_path, "r") as file:
+            csv_reader = csv.reader(file)
+            next(csv_reader)
+
+            for row in csv_reader:
+                d_name, d_price, i_name, i_quantity = (row)
+
+                dish = next((d for d in self.dishes if d.name == d_name), None)
+                if not dish:
+                    dish = Dish(d_name, float(d_price))
+                    self.dishes.add(dish)
+
+                ingredient = Ingredient(i_name)
+                dish.add_ingredient_dependency(
+                    ingredient, int(i_quantity))
